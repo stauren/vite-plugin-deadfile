bee87f59423a56fc4ead65f0abd2ac5fa4a46600
stauren <stauren@qq.com>
2024-01-12 20:26:33 +0800

    add `isDynamicModuleLive` config
    refactor and abstract FileMarker

---
+++ src/file-marker.ts
@@ -0,0 +1,54 @@
+import { promises as fs } from 'node:fs';
+import { resolve } from 'node:path';
+import { createFilter } from 'vite';
+
+function isLegalSource(fileName: string) {
+  if (fileName === '.' || fileName === '..') {
+    return false;
+  }
+  if (fileName === 'node_modules') {
+    return false;
+  }
+  return true;
+}
+
+async function readSourceFiles(root: string, filter: ReturnType<typeof createFilter>) {
+  let result: string[] = [];
+  const level1Sources = await fs.readdir(root);
+  const readAll = level1Sources.filter(isLegalSource).map(async (fileName) => {
+    const subFilePath = resolve(root, fileName);
+    const fileStat = await fs.stat(subFilePath);
+    if (fileStat.isDirectory()) {
+      const subResult = await readSourceFiles(subFilePath, filter);
+      result = [...result, ...subResult];
+    } else if (fileStat.isFile()) {
+      if (filter(subFilePath)) {
+        result.push(subFilePath);
+      }
+    }
+  });
+  await Promise.all(readAll);
+  return result;
+}
+
+export default class FileMarker {
+  public static touchedFiles: Set<string> = new Set();
+  public static sourceFiles: Set<string> = new Set();
+  public static deadFiles: Set<string> = new Set();
+  public static viteDynamicImports: Set<string> = new Set();
+
+  public static async init(root: string, filter: ReturnType<typeof createFilter>) {
+    this.touchedFiles = new Set();
+    this.sourceFiles = new Set(await readSourceFiles(root, filter));
+    this.deadFiles = new Set(this.sourceFiles);
+    this.viteDynamicImports = new Set();
+  }
+  public static touch(id: string) {
+    if (id.indexOf('node_modules') === -1) {
+      if (this.sourceFiles.has(id)) {
+        this.touchedFiles.add(id);
+        this.deadFiles.delete(id);
+      }
+    }
+  }
+}
--- src/visitor.ts
+++ src/visitor.ts
@@ -1,12 +1,10 @@
 import { Visitor } from '@swc/core/Visitor';
-import type { TsType, ImportDeclaration, Program } from '@swc/core';
+import type { TsType, ImportDeclaration, Expression, ArrowFunctionExpression } from '@swc/core';
 
 export class ImportVisitor extends Visitor {
   private imports: string[] = [];
-  public visitProgram(n: Program): Program {
+  public init() {
     this.imports = [];
-    const result = super.visitProgram(n);
-    return result;
   }
   public visitTsType(n: TsType) {
     return n;
@@ -18,5 +16,43 @@ export class ImportVisitor extends Visitor {
   }
   public getImports() {
     return this.imports;
+  }
+}
+
+export class DynamicImportVisitor extends Visitor {
+  private collectViteDynamicImport = false;
+  private viteDynamicImports: string[] = [];
+  public init() {
+    this.viteDynamicImports = [];
+  }
+  public visitTsType(n: TsType) {
+    return n;
+  }
+  public visitArrowFunctionExpression(n: ArrowFunctionExpression): Expression {
+    let isHelper = false;
+    const bd = n.body;
+    if (bd.type === 'CallExpression') {
+      if (bd.callee.type === 'Identifier') {
+        // () => __variableDynamicImportRuntimeHelper((/* #__PURE__ */ Object.assign({"./path/to/file.tsx": () => import("./path/to/file.tsx"),})), `./path/to/${moduleName}.tsx`);
+        if (bd.callee.value === '__variableDynamicImportRuntimeHelper') {
+          isHelper = true;
+          this.collectViteDynamicImport = true;
+        }
+      } else if (bd.callee.type === 'Import' && this.collectViteDynamicImport) {
+        const { expression } = bd.arguments[0];
+        if (expression.type === 'StringLiteral') {
+          this.viteDynamicImports.push(expression.value);
+        }
+      }
+    }
+    const result = super.visitArrowFunctionExpression(n);
+
+    if (isHelper) {
+      this.collectViteDynamicImport = false;
+    }
+    return result;
+  }
+  public getViteDynamicImports() {
+    return this.viteDynamicImports;
   }
 }
